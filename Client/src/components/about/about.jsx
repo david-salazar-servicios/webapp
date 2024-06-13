f73a3a49b836df4e78cdfcfbdd079c2cc8abf52e
david-salazar-servicios <davidsalazarservicios@gmail.com>
2024-06-12 21:47:04 -0600

    about

--- Client/src/components/about/about.jsx
+++ Client/src/components/about/about.jsx
@@ -24,7 +24,11 @@ const AboutUs = () => {
                                 <div className="col-lg-6"><img className="img-fluid rounded mb-5 mb-lg-0" src={historia} alt="..." /></div>
                                 <div className="col-lg-6">
                                     <h2 className="fw-bolder">Nuestra Historia</h2>
-                                    <p className="lead fw-normal text-muted mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto est, ut esse a labore aliquam beatae expedita. Blanditiis impedit numquam libero molestiae et fugit cupiditate, quibusdam expedita, maiores eaque quisquam.</p>
+                                    <p className="lead fw-normal text-muted mb-0" >
+                                        En 2009, Don David Salazar fundó Servicios Residenciales y Comerciales CR LTDA con el objetivo de ofrecer sistemas de riego eficientes para áreas residenciales y comerciales. Al notar que muchas empresas priorizaban las ventas sobre la conservación del agua, decidió enfocarse en soluciones sostenibles.<br /><br />
+                                        Diseñamos proyectos para optimizar el uso del agua, implementando sistemas de riego que reducían el consumo y los costos. Con el tiempo, ampliamos nuestra oferta de servicios para atender diversas necesidades residenciales y comerciales.<br /><br />
+                                        Hoy, Servicios Residenciales y Comerciales CR LTDA ofrece más de 15 servicios, manteniendo un firme compromiso con la calidad, la innovación y la sostenibilidad, asegurando un manejo responsable del agua y contribuyendo al bienestar del planeta.
+                                    </p>
                                 </div>
                             </div>
                         </div>
@@ -35,8 +39,11 @@ const AboutUs = () => {
                             <div className="row gx-5 align-items-center">
                                 <div className="col-lg-6 order-first order-lg-last"><img className="img-fluid rounded mb-5 mb-lg-0" src={David2} alt="..." /></div>
                                 <div className="col-lg-6">
-                                    <h2 className="fw-bolder">Misión, Visión y Valores</h2>
-                                    <p className="lead fw-normal text-muted mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto est, ut esse a labore aliquam beatae expedita. Blanditiis impedit numquam libero molestiae et fugit cupiditate, quibusdam expedita, maiores eaque quisquam.</p>
+                                    <h2 className="fw-bolder">Misión y Visión</h2>
+                                    <p className="lead fw-normal text-muted mb-0">
+                                        Nuestra misión es salvaguardar y optimizar el uso del recurso hídrico en cada proyecto que emprendemos, garantizando soluciones sostenibles y eficientes. Nos dedicamos a atender con prontitud y eficacia las emergencias tanto residenciales como comerciales relacionadas con el agua. Nuestro compromiso incluye resolver complicaciones que los clientes puedan enfrentar con el suministro de agua potable, el manejo de aguas residuales y otros sistemas hídricos. Nos esforzamos por proporcionar un servicio de calidad que asegure la satisfacción y la tranquilidad de nuestros clientes, mediante la implementación de tecnologías avanzadas y prácticas responsables que promuevan la conservación del agua y el bienestar comunitario.<br /><br />
+                                        Nuestra visión es trabajar de la mano con los recursos naturales para maximizar su aprovechamiento, promoviendo una economía más eficiente y sostenible para nuestros consumidores. Aspiramos a ser líderes en soluciones hídricas que no sólo resuelvan problemas inmediatos, sino que también fomenten el uso responsable y consciente del agua, garantizando un futuro donde la eficiencia y la sostenibilidad sean la norma en cada hogar y negocio que servimos.
+                                    </p>
                                 </div>
                             </div>
                         </div>
