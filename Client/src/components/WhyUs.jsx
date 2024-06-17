3f1d527ad901b6313906365a4a9dc91c3288f2fc
“Abiel <abiel2001d@gmail.com>
2024-06-17 17:11:03 -0600

    motions updated

--- Client/src/components/WhyUs.jsx
+++ Client/src/components/WhyUs.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
-
+import image from '../../src/assets/images/why-us-bg.jpg';
 
 
 export default () => {
