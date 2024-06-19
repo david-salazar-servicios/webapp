f8cb384234b49b5027f726cfa7d34cd3f22bea2b
david-salazar-servicios <davidsalazarservicios@gmail.com>
2024-06-19 12:15:23 -0600

    proyectos

--- Client/src/components/proyectos/proyectos.jsx
+++ Client/src/components/proyectos/proyectos.jsx
@@ -1,50 +1,57 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import { Modal, Button } from 'react-bootstrap';
 import { motion } from 'framer-motion';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 import '../../vendor/bootstrap/css/proyectos.css';
+
+import Fontanería_1 from '../../assets/images/Proyectos/Fontanería_1.jpg';
+import Fontanería_2 from '../../assets/images/Proyectos/Fontanería_2.jpg';
+import Fugas_1 from '../../assets/images/Proyectos/Fugas_1.jpg';
+import Fugas_2 from '../../assets/images/Proyectos/Fugas_2.jpg';
+import Riego_1 from '../../assets/images/Proyectos/Riego_1.jpg';
+import Riego_2 from '../../assets/images/Proyectos/Riego_2.jpg';
 
 const images = [
     [
         {
-            src: 'https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp',
-            alt: 'Boat on Calm Water',
+            src: Fontanería_1,
+            alt: 'Trabajos de Fontanería',
             className: 'w-100 shadow-1-strong rounded mb-4 img-hover-effect',
         },
         {
-            src: 'https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain1.webp',
-            alt: 'Wintry Mountain Landscape',
-            className: 'w-100 shadow-1-strong rounded mb-4 img-hover-effect',
-        },
-    ],
-    [
-        {
-            src: 'https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain2.webp',
-            alt: 'Mountains in the Clouds',
-            className: 'w-100 shadow-1-strong rounded mb-4 img-hover-effect',
-        },
-        {
-            src: 'https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp',
-            alt: 'Boat on Calm Water',
+            src: Fugas_1,
+            alt: 'Detección de Fugas',
             className: 'w-100 shadow-1-strong rounded mb-4 img-hover-effect',
         },
     ],
     [
         {
-            src: 'https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(18).webp',
-            alt: 'Waves at Sea',
+            src: Riego_1,
+            alt: 'Sistema de Riego',
             className: 'w-100 shadow-1-strong rounded mb-4 img-hover-effect',
         },
         {
-            src: 'https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain3.webp',
-            alt: 'Yosemite National Park',
+            src: Fontanería_2,
+            alt: 'Instalación de Tuberías',
+            className: 'w-100 shadow-1-strong rounded mb-4 img-hover-effect',
+        },
+    ],
+    [
+        {
+            src: Fugas_2,
+            alt: 'Equipo de Detección de Fugas',
+            className: 'w-100 shadow-1-strong rounded mb-4 img-hover-effect',
+        },
+        {
+            src: Riego_2,
+            alt: 'Sistema de Riego Automático',
             className: 'w-100 shadow-1-strong rounded mb-4 img-hover-effect',
         },
     ],
 ];
 
-const Proyectos = () => {
-    // Define the animation variants
+const Proyectos = React.memo(() => {
     const boxVariants = {
         hidden: { opacity: 0, y: 50 },
         visible: { opacity: 1, y: 0 },
@@ -53,58 +60,65 @@ const Proyectos = () => {
     const [show, setShow] = useState(false);
     const [currentImage, setCurrentImage] = useState({ src: '', alt: '' });
 
-    const handleClose = () => setShow(false);
-    const handleShow = (image) => {
+    const handleClose = useCallback(() => setShow(false), []);
+    const handleShow = useCallback((image) => {
         setCurrentImage(image);
         setShow(true);
-    };
-    return (
-        <> <motion.div initial="hidden" animate="visible" variants={boxVariants}>
-            <section className="experiance-section">
-                <div className="content-column col-lg-7 col-md-12 col-sm-12">
-                    <div className="title-box">
-                        <h2>Galería de Proyectos</h2>
-                    </div>
-                </div>
-                <div className="row">
-                    {images.map((column, colIndex) => (
-                        <div className={`col-lg-4 col-md-12 mb-4 mb-lg-0`} key={colIndex}>
-                            {column.map((image, imgIndex) => (
-                                <img
-                                    key={imgIndex}
-                                    src={image.src}
-                                    className={image.className}
-                                    alt={image.alt}
-                                    onClick={() => handleShow(image)}
-                                    style={{ cursor: 'pointer' }}
-                                />
-                            ))}
-                        </div>
-                    ))}
+    }, []);
 
-                    <Modal show={show} onHide={handleClose}>
-                        <Modal.Header closeButton>
-                            <Modal.Title>{currentImage.alt}</Modal.Title>
-                        </Modal.Header>
-                        <Modal.Body>
-                            <img
-                                src={currentImage.src}
-                                alt={currentImage.alt}
-                                className="w-100"
-                            />
-                        </Modal.Body>
-                        <Modal.Footer>
-                            <Button variant="secondary" onClick={handleClose}>
-                                Close
-                            </Button>
-                        </Modal.Footer>
-                    </Modal>
-                </div>
-            </section>
-        </motion.div>
+    return (
+        <>
+            <motion.div initial="hidden" animate="visible" variants={boxVariants}>
+                <section className="experiance-section">
+                    <div className="content-column col-lg-7 col-md-12 col-sm-12">
+                        <div className="title-box">
+                            <h2>Galería de Proyectos</h2>
+                        </div>
+                    </div>
+                    <div className="row">
+                        {images.map((column, colIndex) => (
+                            <div className={`col-lg-4 col-md-12 mb-4 mb-lg-0`} key={colIndex}>
+                                {column.map((image, imgIndex) => (
+                                    <LazyLoadImage
+                                        key={imgIndex}
+                                        src={image.src}
+                                        className={image.className}
+                                        alt={image.alt}
+                                        onClick={() => handleShow(image)}
+                                        effect="blur"
+                                        style={{ cursor: 'pointer' }}
+                                    />
+                                ))}
+                            </div>
+                        ))}
+                    </div>
+                </section>
+            </motion.div>
+
+            <Modal
+                show={show}
+                onHide={handleClose}
+                dialogClassName="custom-modal"
+                backdropClassName="custom-modal-backdrop"
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>{currentImage.alt}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <img
+                        src={currentImage.src}
+                        alt={currentImage.alt}
+                        className="w-100"
+                    />
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Cerrar
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </>
     );
-};
+});
 
 export default Proyectos;
-
