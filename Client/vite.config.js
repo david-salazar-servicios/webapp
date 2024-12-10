539a1e8303611874aeacddaabbef2df3f0de6790
EMANUEL BERMUDEZ SALGUERA <ebermudez@est.utn.ac.cr>
2024-12-09 20:05:19 -0600

    Lastest

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,16 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  sourcemap: true, // Enable sourcemaps for debugging
-  build: {
-    outDir: 'dist', // Ensure the output directory is 'dist' (default for Vite)
-    sourcemap: true, // Include sourcemaps in the build output
-  },
-  server: {
-    port: 3000, // Optional: Set the development server port
-  },
-  base: './', // Set base to './' for relative paths (useful for Render deployments)
-});
+  sourcemap: true, // Asegúrate de que esto esté habilitado
+})
