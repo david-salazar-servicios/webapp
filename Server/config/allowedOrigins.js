d164bf7f7e5c8de309a981be993a5bc2c2a26459
“Abiel <abiel2001d@gmail.com>
2024-12-09 22:57:21 -0600

    allowedOrigins updated

--- Server/config/allowedOrigins.js
+++ Server/config/allowedOrigins.js
@@ -6,9 +6,7 @@ const allowedOrigins = [
     'http://10.61.207.37:8080',
     'http://localhost:5173',
     'http://127.0.0.1:5173',
-    'https://david-salazar-servicios-backend.onrender.com',
     'https://davidsalazarservicios.com',
-    'https://david-salazar-servicios-frontend.onrender.com'
 ]
 
 module.exports = allowedOrigins;
