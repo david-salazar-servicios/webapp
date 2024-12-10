2e5b62f0996114e53fef6114274250cb2f33839d
EMANUEL BERMUDEZ SALGUERA <ebermudez@est.utn.ac.cr>
2024-12-09 20:36:15 -0600

    changes

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: 'dist',
-  },
-});
-
+  plugins: [react()],
+  sourcemap: true, // Asegúrate de que esto esté habilitado
+})
