8dec9fa71284b005039e3504970d89e4ccaa010a
EMANUEL BERMUDEZ SALGUERA <ebermudez@est.utn.ac.cr>
2024-12-09 20:08:46 -0600

    updated cors

--- Server/config/allowedOrigins.js
+++ Server/config/allowedOrigins.js
@@ -5,7 +5,8 @@ const allowedOrigins = [
     'http://localhost:8080',
     'http://10.61.207.37:8080',
     'http://localhost:5173',
-    'http://127.0.0.1:5173'
+    'http://127.0.0.1:5173',
+    'https://david-salazar-servicios-backend.onrender.com'
 ]
 
 module.exports = allowedOrigins;
