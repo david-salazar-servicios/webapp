82f819ab2636935924e4765f71bb1e9835f07642
EMANUEL BERMUDEZ SALGUERA <ebermudez@est.utn.ac.cr>
2024-12-09 20:27:01 -0600

    Commit

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,17 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  sourcemap: true, // Asegúrate de que esto esté habilitado
+  base: './', // Set to './' for relative paths
   build: {
-    sourcemap: true,
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name].[hash][extname]'
-      }
-    }
-  },
-  server: {
-    strictPort: true, // Ensure the server uses a fixed port
-    open: true // Automatically open the app in the browser
+    outDir: 'dist', // Matches your folder structure
   }
-});
+  
+})
+
