0e5cf8aec96b2d424ad9daa23615e34beafb1388
“Abiel <abiel2001d@gmail.com>
2024-12-09 21:41:19 -0600

    defineConfig updated

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  sourcemap: true, // Asegúrate de que esto esté habilitado
-})
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    historyApiFallback: true, // Ensures proper SPA routing during development
+  },
+});
